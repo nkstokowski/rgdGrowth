cf2f938b3d9009f4d1db51d9403358aa2b8f455d
MichaelT115 <mtc5284@rit.edu>
2018-07-28 12:27:42 -0400

    Add GrowShrink prototype.

---
+++ Assets/ShrinkGrow.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class ShrinkGrow : MonoBehaviour
+{
+    public enum SizeState { SMALL, MEDIUM, LARGE }
+    public SizeState sizeState;
+
+    [Serializable]
+    public class SizeProperties
+    {
+        public float mass;
+        public float maxSpeed;
+        public float height;
+        public float jumpHeight;
+        public float cameraHalfHeight;
+    }
+    public SizeProperties smallProperties;
+    public SizeProperties mediumProperties;
+    public SizeProperties largeProperties;
+
+    protected PlatformerCharacter2D character;
+    protected Rigidbody2D rb2d;
+
+    void Start()
+    {
+        character = GetComponent<PlatformerCharacter2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+        SetSizeProperties(smallProperties);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            sizeState = SizeState.SMALL;
+            SetSizeProperties(smallProperties);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            sizeState = SizeState.MEDIUM;
+            SetSizeProperties(mediumProperties);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            sizeState = SizeState.LARGE;
+            SetSizeProperties(largeProperties);
+        }
+    }
+
+    private void SetSizeProperties(SizeProperties sizeProperties)
+    {
+        rb2d.mass = sizeProperties.mass;
+        character.maxSpeed = sizeProperties.maxSpeed;
+        transform.localScale = new Vector2(sizeProperties.height / 1.75f, sizeProperties.height / 1.75f);
+        character.jumpSpeed = Mathf.Sqrt(sizeProperties.jumpHeight * 2 * Physics2D.gravity.magnitude);
+        Camera.main.orthographicSize = sizeProperties.cameraHalfHeight;
+    }
+}
--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -5,16 +5,16 @@ namespace UnityStandardAssets._2D
 {
     public class PlatformerCharacter2D : MonoBehaviour
     {
-        [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
-        [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
+        [SerializeField] public float maxSpeed = 10f;                    // The fastest the player can travel in the x axis.
+        [SerializeField] public float jumpSpeed = 400f;                  // Amount of force added when the player jumps.
         [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
         [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
 
-        private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
+        public Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-        private bool m_Grounded;            // Whether or not the player is grounded.
-        private Transform m_CeilingCheck;   // A position marking where to check for ceilings
+        [SerializeField] private bool m_Grounded;            // Whether or not the player is grounded.
+        public Transform m_CeilingCheck;   // A position marking where to check for ceilings
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
         private Animator m_Anim;            // Reference to the player's animator component.
         private Rigidbody2D m_Rigidbody2D;
@@ -22,10 +22,7 @@ namespace UnityStandardAssets._2D
 
         private void Awake()
         {
-            // Setting up references.
-            m_GroundCheck = transform.Find("GroundCheck");
-            m_CeilingCheck = transform.Find("CeilingCheck");
-            m_Anim = GetComponent<Animator>();
+            m_Anim = GetComponentInChildren<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
@@ -39,7 +36,7 @@ namespace UnityStandardAssets._2D
             Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
             for (int i = 0; i < colliders.Length; i++)
             {
-                if (colliders[i].gameObject != gameObject)
+                if (!colliders[i].CompareTag("Player"))
                     m_Grounded = true;
             }
             m_Anim.SetBool("Ground", m_Grounded);
@@ -74,7 +71,7 @@ namespace UnityStandardAssets._2D
                 m_Anim.SetFloat("Speed", Mathf.Abs(move));
 
                 // Move the character
-                m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
+                m_Rigidbody2D.velocity = new Vector2(move*maxSpeed, m_Rigidbody2D.velocity.y);
 
                 // If the input is moving the player right and the player is facing left...
                 if (move > 0 && !m_FacingRight)
@@ -95,7 +92,7 @@ namespace UnityStandardAssets._2D
                 // Add a vertical force to the player.
                 m_Grounded = false;
                 m_Anim.SetBool("Ground", false);
-                m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+                m_Rigidbody2D.velocity += new Vector2(0f, jumpSpeed);
             }
         }
 
