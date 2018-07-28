5176dac7abdd68c96190f3ef6dd6b447c3ced11a
MichaelT115 <mtc5284@rit.edu>
2018-07-28 14:00:08 -0400

    The player checks if it can grow before changing size.

--- Assets/Scripts/Player/ShrinkGrow.cs
+++ Assets/Scripts/Player/ShrinkGrow.cs
@@ -37,18 +37,78 @@ public class ShrinkGrow : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            sizeState = SizeState.SMALL;
-            SetSizeProperties(smallProperties);
+            SetSizeState(SizeState.SMALL);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            sizeState = SizeState.MEDIUM;
-            SetSizeProperties(mediumProperties);
+            SetSizeState(SizeState.MEDIUM);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            sizeState = SizeState.LARGE;
-            SetSizeProperties(largeProperties);
+            SetSizeState(SizeState.LARGE);
+        }
+    }
+
+    private bool CanPlayerGrowToThisSize(SizeProperties sizeProperties)
+    {
+        float height = sizeProperties.height;
+        float halfHeight = height / 2;
+        float width = sizeProperties.height * 0.30f;
+        float halfWidth = width / 2;
+
+
+        Vector2 center = (Vector2)transform.position + new Vector2(0, halfHeight);
+        Vector2 size = new Vector2(width - 0.2f, height - 0.2f);
+        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(center, size, 0);
+
+        #region Debug
+        const int Duration = 5;
+        Vector2 halfSize = new Vector2(halfWidth, halfHeight);
+
+        Vector2 topLeftCorner = new Vector2(center.x - halfSize.x, center.y + halfSize.y);
+        Vector2 topRightCorner = new Vector2(center.x + halfSize.x, center.y + halfSize.y);
+        Vector2 bottomLeftCorner = new Vector2(center.x - halfSize.x, center.y - halfSize.y);
+        Vector2 bottomRightCorner = new Vector2(center.x + halfSize.x, center.y - halfSize.y);
+
+        Debug.DrawLine(topLeftCorner, topRightCorner, Color.gray, Duration);
+        Debug.DrawLine(topRightCorner, bottomRightCorner, Color.gray, Duration);
+        Debug.DrawLine(bottomRightCorner, bottomLeftCorner, Color.gray, Duration);
+        Debug.DrawLine(bottomLeftCorner, topLeftCorner, Color.gray, Duration);
+        #endregion
+
+        for (int i = 0; i < hitColliders.Length; ++i)
+        {
+            if (!hitColliders[i].CompareTag("Player"))
+                return false;
+        }
+        return true;
+    }
+
+    private void SetSizeState( SizeState sizeState)
+    {
+        switch (sizeState)
+        {
+            case SizeState.SMALL:
+                if (CanPlayerGrowToThisSize(smallProperties))
+                {
+                    SetSizeProperties(smallProperties);
+                    this.sizeState = SizeState.SMALL;
+                }
+                break;
+            case SizeState.MEDIUM:
+                if (CanPlayerGrowToThisSize(mediumProperties))
+                {
+                    SetSizeProperties(mediumProperties);
+                    this.sizeState = SizeState.MEDIUM;
+                }
+                break;
+            case SizeState.LARGE:
+                if (CanPlayerGrowToThisSize(largeProperties))
+                {
+                    SetSizeProperties(largeProperties);
+                    this.sizeState = SizeState.LARGE;
+                }
+                break;
         }
     }
 
@@ -56,7 +116,7 @@ public class ShrinkGrow : MonoBehaviour
     {
         rb2d.mass = sizeProperties.mass;
         character.maxSpeed = sizeProperties.maxSpeed;
-        transform.localScale = new Vector2(sizeProperties.height / 1.75f, sizeProperties.height / 1.75f);
+        transform.localScale = new Vector2(sizeProperties.height, sizeProperties.height);
         character.jumpSpeed = Mathf.Sqrt(sizeProperties.jumpHeight * 2 * Physics2D.gravity.magnitude);
         Camera.main.orthographicSize = sizeProperties.cameraHalfHeight;
     }
